915a299c583f9688bb6f11c206a8f4f001d6a474
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2021-10-14 15:37:11 -0500

    Simple card duplicated

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='card'>
+        <img
+          src='http://sitesnobrasil.com/imagens-fotos/homens/h/homero-simpson2.jpg'
+          alt='Homer S'
+        />
+        <h3>Homer Simpson</h3>
+        <p>Description</p>
+      </div>
+      <div className='card'>
+        <img
+          src='http://sitesnobrasil.com/imagens-fotos/homens/h/homero-simpson2.jpg'
+          alt='Homer S'
+        />
+        <h3>Homer Simpson</h3>
+        <p>Description</p>
+      </div>
+      <div className='card'>
+        <img
+          src='http://sitesnobrasil.com/imagens-fotos/homens/h/homero-simpson2.jpg'
+          alt='Homer S'
+        />
+        <h3>Homer Simpson</h3>
+        <p>Description</p>
+      </div>
     </div>
   );
 }
