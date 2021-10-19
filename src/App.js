d485e0b89b1b9885ac790efde9be0a10992539a9
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2021-10-18 22:51:46 -0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.scss';
 import { ContactForm } from './components/contact-form/ContactForm';
 import { Gallery } from './components/gallery/Gallery';
+import NavBar from './components/navbar/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <h1>Rick and Morty</h1>
-      <Gallery />
-      <ContactForm />
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path='/gallery'>
+            <Gallery />
+          </Route>
+          <Route exact path='/contact-form'>
+            <ContactForm />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <a className='navbar-brand' href='#main'>
+        ISIS3710
+      </a>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNav'
+        aria-controls='navbarNav'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarNav'>
+        <ul className='navbar-nav'>
+          <li className='nav-item active'>
+            <Link className='nav-link' to='/gallery'>
+              Gallery
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/contact-form'>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
